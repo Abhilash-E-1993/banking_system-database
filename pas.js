421d9d3306bbbfa2883fa3b7babfe904a72991ab
Abhilash-E-1993 <edigaabhilash2@gmail.com>
2025-09-06 11:33:32 +0530

    half_done

--- pas.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-const newPassword = 'dbmsproject';
-
-bcrypt.hash(newPassword, 10, (err, hash) => {
-  if (err) throw err;
-  console.log('New Hashed Password:', hash);
-});
